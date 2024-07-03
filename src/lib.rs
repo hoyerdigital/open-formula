a7a55fe168b6f01803c2c88d94e073a2f637b59c
Niclas Hoyer <info@niclashoyer.de>
2024-07-04 00:51:15 +0200

    cell ref

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use enum_as_inner::EnumAsInner;
 
 pub mod parser;
+pub mod sql;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Comp {
@@ -29,4 +30,45 @@ pub enum Expr {
     Func(String, Vec<Self>),
     CellRef(String, usize),
     CellRange((String, usize), (String, usize)),
+}
+
+pub fn column_to_id<S: AsRef<str>>(col: S) -> Result<usize, ()> {
+    let col = col.as_ref();
+    let len = col.len();
+    if len == 0 {
+        return Err(());
+    }
+    let col = col.to_uppercase().chars().collect::<String>();
+    if !col.chars().all(|x| char::is_ascii_uppercase(&x)) {
+        return Err(());
+    }
+    let mut sum = 0;
+    for c in col.chars() {
+        sum *= 26;
+        sum += (c as u8) as usize - 65 + 1;
+    }
+    // return sum - 1 to let idx start at 0
+    Ok(sum - 1)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn column_ids() {
+        assert_eq!(column_to_id("A"), Ok(0));
+        assert_eq!(column_to_id("B"), Ok(1));
+        assert_eq!(column_to_id("C"), Ok(2));
+        assert_eq!(column_to_id("AA"), Ok(26));
+        assert_eq!(column_to_id("Aa"), Ok(26));
+        assert_eq!(column_to_id("aA"), Ok(26));
+        assert_eq!(column_to_id("ab"), Ok(27));
+        assert_eq!(column_to_id("AB"), Ok(27));
+        assert_eq!(column_to_id("XFD"), Ok(16383));
+
+        assert_eq!(column_to_id(""), Err(()));
+        assert_eq!(column_to_id("%"), Err(()));
+        assert_eq!(column_to_id("A0F"), Err(()));
+    }
 }
---
+++ src/sql.rs
@@ -0,0 +1,156 @@
+use crate::{Comp, Expr};
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    ColumnIndexOutOfBounds,
+    MultipleRowsReferenced,
+    MalformedCellIndex,
+}
+
+#[derive(Debug)]
+pub struct Context<'a> {
+    columns: &'a Vec<String>,
+    row: Option<usize>,
+}
+
+fn comp_sql(c: &Comp) -> &'static str {
+    match c {
+        Comp::Equal => "=",
+        Comp::NotEqual => "<>",
+        Comp::Greater => ">",
+        Comp::Lower => "<",
+        Comp::GreaterEqual => ">=",
+        Comp::LowerEqual => "<=",
+    }
+}
+
+pub fn transform<'a>(expr: &'a Expr) -> Result<String, Error> {
+    transform_with_columns(expr, &vec![])
+}
+
+pub fn transform_with_columns<'a>(expr: &'a Expr, columns: &Vec<String>) -> Result<String, Error> {
+    let mut ctx = Context { columns, row: None };
+    transform_(expr, &mut ctx)
+}
+
+fn transform_<'a>(expr: &'a Expr, ctx: &mut Context) -> Result<String, Error> {
+    match expr {
+        Expr::Num(n) => Ok(format!("{:}", n)),
+        Expr::Bool(b) => {
+            if *b {
+                Ok("1".into())
+            } else {
+                Ok("0".into())
+            }
+        }
+        Expr::String(s) => Ok(format!("'{}'", s.replace("'", "''"))),
+        Expr::Perc(a) => Ok(format!("({}/100.0)", transform_(a, ctx)?)),
+        Expr::Neg(a) => Ok(format!("-{}", transform_(a, ctx)?)),
+        Expr::Add(a, b) => Ok(format!("{} + {}", transform_(a, ctx)?, transform_(b, ctx)?)),
+        Expr::Sub(a, b) => Ok(format!("{} - {}", transform_(a, ctx)?, transform_(b, ctx)?)),
+        Expr::Mul(a, b) => Ok(format!("{} * {}", transform_(a, ctx)?, transform_(b, ctx)?)),
+        Expr::Div(a, b) => Ok(format!("{} / {}", transform_(a, ctx)?, transform_(b, ctx)?)),
+        Expr::Pow(a, b) => Ok(format!(
+            "POW({}, {})",
+            transform_(a, ctx)?,
+            transform_(b, ctx)?
+        )),
+        Expr::Concat(a, b) => Ok(format!(
+            "CONCAT({}, {})",
+            transform_(a, ctx)?,
+            transform_(b, ctx)?
+        )),
+        Expr::Cond(c, a, b) => Ok(format!(
+            "({} {} {})",
+            transform_(a, ctx)?,
+            comp_sql(c),
+            transform_(b, ctx)?
+        )),
+        Expr::CellRef(cell, num) => {
+            let id = crate::column_to_id(cell).map_err(|_| Error::MalformedCellIndex)?;
+            // check if index is out of bounds
+            if id + 1 > ctx.columns.len() {
+                return Err(Error::ColumnIndexOutOfBounds);
+            }
+            // check if all references are on the same row
+            if let Some(n) = ctx.row {
+                if n != *num {
+                    return Err(Error::MultipleRowsReferenced);
+                }
+            } else {
+                ctx.row = Some(*num);
+            }
+
+            Ok(ctx.columns.get(id).unwrap().clone())
+        }
+        _ => {
+            todo!()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple() {
+        assert_eq!(transform(&Expr::Bool(false)).unwrap(), "0");
+        assert_eq!(transform(&Expr::Bool(true)).unwrap(), "1");
+        assert_eq!(transform(&Expr::Num(3.0)).unwrap(), "3");
+        assert_eq!(
+            transform(&Expr::Neg(Box::new(Expr::Num(3.0)))).unwrap(),
+            "-3"
+        );
+    }
+
+    #[test]
+    fn cell_ref() {
+        let cols = ["foo", "bar", "baz"].map(String::from).to_vec();
+        let check_ref = |a: &'static str, b| {
+            assert_eq!(
+                transform_with_columns(&Expr::CellRef(a.into(), 1), &cols).unwrap(),
+                b
+            );
+        };
+        check_ref("A", "foo");
+        check_ref("B", "bar");
+        check_ref("C", "baz");
+    }
+
+    #[test]
+    fn cell_ref_invalid() {
+        let cols = ["foo", "bar", "baz"].map(String::from).to_vec();
+        let check_ref = |col: &'static str, row: usize, e: Error| {
+            assert_eq!(
+                transform_with_columns(&Expr::CellRef(col.into(), row), &cols),
+                Err(e)
+            );
+        };
+        check_ref("X", 1, Error::ColumnIndexOutOfBounds);
+        check_ref("AX", 3, Error::ColumnIndexOutOfBounds);
+        check_ref("XA", 5, Error::ColumnIndexOutOfBounds);
+        check_ref("aa", 42, Error::ColumnIndexOutOfBounds);
+        check_ref("%A", 23, Error::MalformedCellIndex);
+        check_ref("A&", 23, Error::MalformedCellIndex);
+        check_ref("!", 23, Error::MalformedCellIndex);
+    }
+
+    #[test]
+    fn cell_ref_multiple_rows() {
+        let cols = ["foo", "bar", "baz"].map(String::from).to_vec();
+        let check_refs = |rows: Vec<usize>, e: Result<String, Error>| {
+            let expr = rows.iter().fold(Expr::Num(0.0), |sum, x| {
+                Expr::Add(Box::new(sum), Box::new(Expr::CellRef("A".into(), *x)))
+            });
+            assert_eq!(transform_with_columns(&expr, &cols), e);
+        };
+        check_refs(vec![1], Ok("0 + foo".into()));
+        check_refs(vec![1, 1], Ok("0 + foo + foo".into()));
+        check_refs(vec![3, 3], Ok("0 + foo + foo".into()));
+        check_refs(vec![4, 4, 4, 4], Ok("0 + foo + foo + foo + foo".into()));
+        check_refs(vec![1, 2], Err(Error::MultipleRowsReferenced));
+        check_refs(vec![3, 1], Err(Error::MultipleRowsReferenced));
+        check_refs(vec![1, 1, 1, 3], Err(Error::MultipleRowsReferenced));
+    }
+}
