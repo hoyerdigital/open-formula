883c92b89c5700d875c84021e67d4e6d9603ce4f
Niclas Hoyer <info@niclashoyer.de>
2024-11-03 19:40:24 +0100

    optimize column character conversion

--- src/helpers.rs
+++ src/helpers.rs
@@ -10,12 +10,11 @@ pub fn column_to_id<S: AsRef<str>>(col: S) -> Result<usize, Error> {
     if len == 0 {
         return Err(Error::EmptyReference);
     }
-    let col = col.to_uppercase().chars().collect::<String>();
-    if !col.chars().all(|x| char::is_ascii_uppercase(&x)) {
-        return Err(Error::MalformedReference);
-    }
     let mut sum = 0;
     for c in col.chars() {
+        if !c.is_ascii_uppercase() {
+            return Err(Error::MalformedReference);
+        }
         sum *= 26;
         sum += (c as u8) as usize - 65 + 1;
     }
@@ -32,14 +31,14 @@ mod tests {
         assert_eq!(column_to_id("B"), Ok(2));
         assert_eq!(column_to_id("C"), Ok(3));
         assert_eq!(column_to_id("AA"), Ok(27));
-        assert_eq!(column_to_id("Aa"), Ok(27));
-        assert_eq!(column_to_id("aA"), Ok(27));
-        assert_eq!(column_to_id("ab"), Ok(28));
         assert_eq!(column_to_id("AB"), Ok(28));
         assert_eq!(column_to_id("XFD"), Ok(16384));
 
         assert_eq!(column_to_id(""), Err(Error::EmptyReference));
         assert_eq!(column_to_id("%"), Err(Error::MalformedReference));
         assert_eq!(column_to_id("A0F"), Err(Error::MalformedReference));
+        assert_eq!(column_to_id("Aa"), Err(Error::MalformedReference));
+        assert_eq!(column_to_id("aA"), Err(Error::MalformedReference));
+        assert_eq!(column_to_id("ab"), Err(Error::MalformedReference));
     }
 }
